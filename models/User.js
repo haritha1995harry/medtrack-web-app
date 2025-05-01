3ace9c2fc3eb34df5b43882e6accb0bf8217536b
Piyu HP Binu <mr.binara@gmail.com>
2025-05-01 23:56:09 +1000

    mongodb setup and user schema with pw encryption

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: Date, required: true },
+    gender: { type: String, enum: ['male', 'female', 'other'], required: true },
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    contactNumber: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express")
 var app = express()
+const mongoose = require('mongoose');
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/medtrackDB');
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
+
 
 var port = process.env.port || 3000;
 app.listen(port, () => {
